3ba42aacd8284239a5aa7a6fdef87d0313b5d7f5
cayote <klotilda.coskun@gmail.com>
2020-06-18 09:15:15 -0700

    logical Operators

---
+++ TildaEx/EX/LogicalOperators.js
@@ -0,0 +1,31 @@
+console.log(true&&true);
+console.log(true&&false);
+console.log(true||true);
+console.log(true||false);
+
+console.log(true&&"Machine");
+console.log(false&&"Machine");
+console.log(true||"Machine");
+console.log(false||"Machine");
+
+console.log(1+1==2 && 10*10>50);
+console.log(6*null);
+console.log("5"-1);//4
+console.log("5"+1);//51--> "+" is concatenation operator
+console.log("5"-(5+1));// -1
+
+console.log(true==1);
+console.log(false==0);
+console.log(null == undefined);
+console.log(0==undefined);
+console.log(0==null);
+console.log(null&&"user");
+console.log("user"&&null);
+console.log(null||"user");
+console.log("user"||null);
+
+console.log("value"&&"user");
+console.log("user"&&"value");
+console.log("value"||"user");
+console.log("user"||"value");
+
