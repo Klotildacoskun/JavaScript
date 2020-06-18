b1c60833ea9b62fcc62a59729e792da784d83ce0
cayote <klotilda.coskun@gmail.com>
2020-06-18 08:51:22 -0700

    twoLinePrint and  print

---
+++ TildaEx/EX/twoLinePrint.js
@@ -0,0 +1,9 @@
+console.log("This text will print in one line");
+console.log("This text \n will print in two lines");
+console.log("This text has double slashes printed like \"try\" ");
+//single quotes used to print double quotes
+console.log('This text has double slashes printed like "try" ');
+//concatination
+console.log("con"+"ca"+"ti"+"nation");
+
+
